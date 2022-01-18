50f1ee21ab99e9769a8a50587d1dae20435bf4fb
dyu <david.yu.ftw@gmail.com>
2022-01-18 23:04:08 +0800

    endian check

--- src/endian.cpp
+++ src/endian.cpp
@@ -9,13 +9,21 @@ enum endian_t {
 enum { OS_BYTE_ORDER = OS_BIG_ENDIAN };
 #elif defined(__LITTLE_ENDIAN__)
 enum { OS_BYTE_ORDER = OS_LITTLE_ENDIAN };
+#elif defined(_WIN32)
+
+#if defined(_ppc_) || defined(_ppc64_) || defined(__ppc__) || defined(__ppc64__) || defined(__POWERPC__) || defined(_M_PPC)
+enum { OS_BYTE_ORDER = OS_BIG_ENDIAN };
+#else
+enum { OS_BYTE_ORDER = OS_LITTLE_ENDIAN };
+#endif
+
 #else
 
 #if defined(BSD)
 #include <sys/endian.h>
-#else // BSD
+#else
 #include <endian.h>
-#endif // BSD
+#endif
 
 #if __BYTE_ORDER == __BIG_ENDIAN
 enum { OS_BYTE_ORDER = OS_BIG_ENDIAN };
@@ -30,5 +38,9 @@ enum { OS_BYTE_ORDER = OS_LITTLE_ENDIAN };
 #include <stdio.h>
 
 int main() {
-    printf("little endian: %d\n", OS_BYTE_ORDER == OS_LITTLE_ENDIAN);
+    auto little = OS_BYTE_ORDER == OS_LITTLE_ENDIAN;
+    printf("little endian: %d\n", little);
+    #if defined(__LITTLE_ENDIAN__)
+    printf("__LITTLE_ENDIAN__: 1\n");
+    #endif
 }
