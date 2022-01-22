07572b68dbbf3c043ec0f27fc3993896d61e822c
dyu <david.yu.ftw@gmail.com>
2022-01-22 17:56:34 +0800

    handle win32

--- run-tests.sh
+++ run-tests.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-TESTS=`ls gn-out/*_test`
+set -e
+
+UNAME=`uname`
+EXE_SUFFIX=""
+[ "$UNAME" != "Linux" ] && [ "$UNAME" != "Darwin" ] && EXE_SUFFIX='.exe'
+TESTS=`ls gn-out/*_test$EXE_SUFFIX`
 
 for T in $TESTS; do
     echo '\n# =================================================='
