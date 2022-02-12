42b97de71d1483f22d72ffcda45748bf0076db67
dyu <david.yu.ftw@gmail.com>
2022-02-12 22:24:01 +0800

    run tests by prefix if specified

--- run-tests.sh
+++ run-tests.sh
@@ -5,7 +5,7 @@ set -e
 UNAME=`uname`
 EXE_SUFFIX=""
 [ "$UNAME" != "Linux" ] && [ "$UNAME" != "Darwin" ] && EXE_SUFFIX='.exe'
-TESTS=`ls gn-out/*_test$EXE_SUFFIX`
+TESTS=`ls gn-out/$1*_test$EXE_SUFFIX`
 
 for T in $TESTS; do
     echo '\n# =================================================='
