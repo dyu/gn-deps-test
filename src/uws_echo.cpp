aa9e971caaf82f85376f507b0af2f8ab0f908271
dyu <david.yu.ftw@gmail.com>
2022-02-24 22:13:25 +0800

    http GET handler

--- src/uws_echo.cpp
+++ src/uws_echo.cpp
@@ -13,6 +13,10 @@ int main(int argc, char** argv)
     h.onMessage([](uWS::WebSocket<uWS::SERVER> *ws, char *message, size_t length, uWS::OpCode opCode) {
         ws->send(message, length, opCode);
     });
+    
+    h.onHttpRequest([](uWS::HttpResponse *res, uWS::HttpRequest req, char *data, size_t length, size_t remainingBytes) {
+        res->end("hello get", 9);
+    });
 
     h.listen("127.0.0.1", port);
     h.run();
